7169ed09d2ab7cd707ff2f44bfb2ddb866d15047
Braulio Rodrigo <brh_braulio@outlook.com>
2020-11-11 00:53:26 -0500

    tarea por hacer

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+        <title>Practica php</title>
+        <link type="text/css" href="css_login.css" rel="stylesheet"/>
+    </head>
+    <body>
+        <form>
+            <input type="text" placeholder="Usuario">
+            <input type="text">
+            <button type="submit">Ingresar</button>
+            <button type="submit">Refrescar</button>
+        </form>
+    </body>
+</html>
